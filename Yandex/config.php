c55c690af41b43c8fe36823f054f9acb0a3bef37
thanh tran <s3491430@student.rmit.edu.au>
2015-05-01 10:09:58 +1000

    remove config file

--- Yandex/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$config = array(
-		"key" => array(
-			"translateKey" => "trnsl.1.1.20150418T042234Z.b126971351aa50bd.6cf3843f5b4f91390cc57c0289179fe4b9dc60ac",
-			"dictionaryKey" => "dict.1.1.20150422T115101Z.5abcc685b6b7f362.4a6c7f76c9e9471ddb118386839ab77d55429167"
-			)
-		);
-?>
